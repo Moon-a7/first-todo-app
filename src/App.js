3fabedf5a51ad268705c16a9d649393d464badfd
Monika Teslak <monika.teslak@gmail.com>
2021-07-31 15:41:10 +0200

    good version

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { useState } from 'react'
 import Form from './components/Form'
-import TodoList from './components/TodoApp'
+import TodoList from './components/TodoList'
+
 import './App.css'
 
 function App() {
-  const [inputText, setInputText] = useState('
-  ')
   return (
     <div className="App">
       <header>
-        <h3>Moni's App</h3>
+        <h3>Moni's Todo List</h3>
       </header>
-      <Form></Form>
-      <TodoList></TodoList>
+      <Form />
+      <TodoList />
     </div>
   )
 }
