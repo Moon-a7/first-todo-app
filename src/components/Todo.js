48033412bdd443ce34d91b8511b5c7a3ff9294fb
Monika Teslak <monika.teslak@gmail.com>
2021-08-06 15:46:58 +0200

    correct Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -8,23 +8,25 @@ const completeHandler = () => {
           ...item, completed: !item.completed
         }
       }
- return item
-    })
-  // const deleteHandler = ({setTodos(todos.filter(el))}) => {
-  }; 
-return (
-    <div className="todo"> 
-          <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
-            
-            {text}</li>
-    
-          <button onClick={completeHandler} className="complete-btn">
-            <i className="fas fa-check"></i>
-          </button>
-          <button onClick={deleteHandler} className="trash-btn">
-            <i className="fas fa-trash"></i>
-          </button>
-        </div> 
-  )
+      const deleteHandler = ({setTodos(todos.filter()}) => {
+        return item
+      }
+
+ 
+ return (
+  <div className="todo"> 
+        <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
+          
+          {text}</li>
+  
+        <button onClick={completeHandler} className="complete-btn">
+          <i className="fas fa-check"></i>
+        </button>
+        <button onClick={deleteHandler} className="trash-btn">
+          <i className="fas fa-trash"></i>
+        </button>
+      </div> 
+)
+
 
 export default Todo
